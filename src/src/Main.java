54027f2df3a0f5a4a0f19b6243627f7e170efdf2
Alice Woodard <a.woodard2386@student.leedsbeckett.ac.uk>
2024-05-10 16:30:17 +0100

    finished

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,9 @@
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import uk.ac.leedsbeckett.oop.OOPGraphics;
 
@@ -14,61 +18,264 @@ public class Main extends OOPGraphics {
         MainFrame.add(this);
         MainFrame.pack();
         MainFrame.setVisible(true);
-        this.about();
+        //this.about();
     }
 
+    @Override
+    public void about(){
+        displayMessage("Alice Woodard");
+        super.about();
+    }
+
+
+
+    StringBuilder commandSaver = new StringBuilder();
+
     public void processCommand(String command) {
-        System.out.println(command);
-        String[] amount = command.split("<",">");
+        System.out.println("Command Entered: "+command);
+        commandSaver.append(command).append("\n");
+        System.out.println(commandSaver);
+        String[] amount = command.split(" ");
         int value = 0;
-        if (amount.length == 2) {
-            command = amount[0];
-            value = Integer.parseInt(amount[1]);
+        int value1 = 0;
+        int value2 = 0;
+        int value3 = 0;
+        float angle1 =0;
+        float angle2 =0;
+        float angle3 =0;
+        String cmd = amount[0];
+        Color colour = new Color(value1,value2,value3);
+
+
+        try {
+            if (amount.length > 1) {
+                if (amount.length == 2) {
+                    value = Integer.parseInt(amount[1]);
+                } else if (amount.length == 3) {
+                    value1 = Integer.parseInt(amount[1]);
+                    value2 = Integer.parseInt(amount[2]);
+                } else if (amount.length == 4) {
+                    value1 = Integer.parseInt(amount[1]);
+                    value2 = Integer.parseInt(amount[2]);
+                    value3 = Integer.parseInt(amount[3]);
+                    colour = new Color(value1,value2,value3);
+
+                }
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid parameter - must be a number!");
         }
 
-
-
-        if (command.equals("penup")) //Lifts the pen from the canvas, so that movement does not get shown.
+        if (cmd.equals("penup")) //Lifts the pen from the canvas, so that movement does not get shown.
         {
             penUp();
         }
-        else if (command.equals("pendown")) //Places the pen down on the canvas so movement gets shown as a drawn line.
+        else if (cmd.equals("pendown")) //Places the pen down on the canvas so movement gets shown as a drawn line.
         {
             penDown();
         }
-        else if (command.equals("turnleft"))
+
+        else if (cmd.equals("black")) //Sets the output pen colour to black.
         {
-            turnLeft(value);
-        }
-        else if (command.equals("turnright")){
-            turnRight(value);
-        }
-        else if (command.equals("forward")){
-            forward(value);
-        }
-        else if (command.equals("backward")){
-            forward((value)*-1);
-        }
-        else if (command.equals("black")){
             setPenColour(Color.black);
         }
-        else if (command.equals("green")){
+        else if (cmd.equals("green")) //Sets the output pen colour to green.
+        {
             setPenColour(Color.green);
         }
-        else if (command.equals("red")){
+        else if (cmd.equals("red")) //Sets the output pen colour to red.
+        {
             setPenColour(Color.red);
         }
-        else if (command.equals("white")){
+        else if (cmd.equals("white")) //Sets the output pen colour to white.
+        {
             setPenColour(Color.white);
         }
-        else if (command.equals("reset")){
+        else if (cmd.equals("reset")) //Resets the canvas to its initial state with turtle pointing down but does not clear the display.
+        {
             reset();
         }
-        else if (command.equals("clear")) {
+        else if (cmd.equals("clear")) //Clears the display.
+        {
             clear();
         }
-        else{
+        else if (cmd.equals("turnleft")) //Turn <degrees> to the left
+        {
+            if (amount.length ==1) {
+                System.out.println("This function needs a parameter!");
+            }
+            else if (value >= 0 && value <= 300) {
+                turnLeft(value);
+            }
+            else {
+                System.out.println("Invalid parameter");
+            }
+        }
+        else if (cmd.equals("turnright")) //Turn <degrees> to the right
+        {
+            if (amount.length ==1) {
+                System.out.println("This function needs a parameter!");
+            }
+            else if (value >= 0 && value <= 300) {
+                turnRight(value);
+            } else {
+                System.out.println("Invalid parameter");
+            }
+        }
+        else if (cmd.equals("forward")) //Move forward the specified distance.
+        {
+            if (amount.length ==1) {
+                System.out.println("This function needs a parameter!");
+            }
+            else if (value >= 0 && value <= 300) {
+                forward(value);
+            }
+            else {
+                System.out.println("Invalid parameter");
+            }
+        }
+        else if (cmd.equals("backward")) //Move backwards the specified distance.
+        {
+            if (amount.length ==1) {
+                System.out.println("This function needs a parameter!");
+            }
+            else if (value >= 0 && value <= 300) {
+                forward((value)*-1);
+            } else {
+                System.out.println("Invalid parameter");
+            }
+        }
+        else if (cmd.equals("about")){
+            about();
+        }
+        else if (cmd.equals("square")){
+            if (amount.length ==1) {
+                System.out.println("This function needs a parameter!");
+            }
+            else if (value >= 0 && value <= 300) {
+                forward(value);
+                turnLeft(90);
+                forward(value);
+                turnLeft(90);
+                forward(value);
+                turnLeft(90);
+                forward(value);
 
+            }
+            else {
+                System.out.println("Invalid parameter");
+            }
+        }
+        else if (cmd.equals("penwidth")){
+            if (amount.length ==1) {
+                System.out.println("This function needs a parameter!");
+            }
+            else if (value >= 0 && value <= 300) {
+                setStroke(value);
+            }
+            else {
+                System.out.println("Invalid parameter");
+            }
+        }
+        else if (cmd.equals("triangle")){
+            if (amount.length ==1) {
+                System.out.println("This function needs a parameter!");
+            }
+            else if (amount.length ==2) {
+                if (value >= 0 && value <= 300) {
+                    forward(value);
+                    turnLeft(120);
+                    forward(value);
+                    turnLeft(120);
+                    forward(value);
+                }
+
+            }
+
+            else {
+                System.out.println("Invalid parameter");
+            }
+        }
+
+        else if (cmd.equals("pencolour")) {
+            if (amount.length == 1) {
+                System.out.println("This function needs three parameters!");
+            }
+            else if (amount.length == 2) {
+                System.out.println("This function needs three parameters!");
+            }
+            else if (amount.length == 3) {
+                System.out.println("This function needs three parameters!");
+            }
+            else if (amount.length == 4)
+            {
+                if (value1 >= 0 && value1 <= 255 || value2 >= 0 && value2 <= 255 || value3 >= 0 && value3 <= 255)
+                {
+                    setPenColour(colour);
+                }
+                else
+                {
+                    System.out.println("Invalid parameter");
+                }
+            }
+            else
+            {
+                System.out.println("Invalid parameter");
+            }
+        }
+
+        else if (cmd.equals("savecommands")){
+            try
+            {
+                // used https://www.w3schools.com/java/java_files_create.asp as a template for this part to help save the file
+                FileWriter fileWriter = new FileWriter("savedCommands.txt");
+                fileWriter.write(String.valueOf(commandSaver));
+                fileWriter.close();
+                System.out.println("Successfully wrote to the file.");
+            }
+            catch (IOException e)
+            {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+        else if (cmd.equals("loadcommands")){
+
+            try {
+                File savedCommands = new File("savedCommands.txt");
+                Scanner fileReader = new Scanner(savedCommands);
+                commandSaver.delete(0, 10000);
+                while (fileReader.hasNextLine())
+                {
+                    String data = fileReader.nextLine();
+                    processCommand(data);
+
+                }
+                fileReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+
+        else if (cmd.equals("saveimage")) {
+            try {
+                BufferedImage image = getBufferedImage();
+                File outputFile = new File("output.png");
+                ImageIO.write(image, "png", outputFile);
+                System.out.println("Image saved as output.png");
+
+            } catch (IOException e) {
+                System.out.println("An error occurred while saving the image.");
+                e.printStackTrace();
+            }
+        }
+
+        else{
+            System.out.println("Unrecognized command");
         }
     }
+
+
 }
